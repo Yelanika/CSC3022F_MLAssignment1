80864ccb7441b73d9362527761221ebc08e81de8
Yelanika <yelanika.gunasekara@gmail.com>
2021-05-19 18:54:11 +0200

    Comparsion of points to centroids complete

--- kmeans.py
+++ kmeans.py
@@ -26,11 +26,6 @@ cluster2_y = y[3]
 cluster3_x = x[6]
 cluster3_y = y[6]
 
-#the cluster lists contains points in that cluster
-cluster1_list = []
-cluster2_list = []
-cluster3_list = []
-
 #eculdian distance for each centroid
 e_distance_cluster1 = 0.0
 e_distance_cluster2 = 0.0
@@ -38,11 +33,44 @@ e_distance_cluster3 = 0.0
 
 #convergence check variables
 converge = False
-converge_prev_value = 0.0
-converge_current_value = 0.0
+
+converge_prev_value_1_x = 0.0
+converge_prev_value_1_y = 0.0
+
+converge_prev_value_2_x = 0.0
+converge_prev_value_2_y = 0.0
+
+converge_prev_value_3_x = 0.0
+converge_prev_value_3_y = 0.0
+
+converge_current_value_1_x = 0.0
+converge_current_value_1_y = 0.0
+
+converge_current_value_2_x = 0.0
+converge_current_value_2_y = 0.0
+
+converge_current_value_3_x = 0.0
+converge_current_value_3_y = 0.0
+
+#iteration counter
+iteration = 1
 
 #finding cluster groupings
 while (converge == False):
+
+    converge_prev_value_1_x = converge_current_value_1_x
+    converge_prev_value_1_y = converge_current_value_1_y
+
+    converge_prev_value_2_x = converge_current_value_2_x
+    converge_prev_value_2_y = converge_current_value_2_y
+
+    converge_prev_value_3_x = converge_current_value_3_x
+    converge_prev_value_3_y = converge_current_value_3_y
+
+    #the cluster lists contains points in that cluster
+    cluster1_list = []
+    cluster2_list = []
+    cluster3_list = []
 
     #comparing each point to centroids
     for c in range(0,8):
@@ -64,11 +92,38 @@ while (converge == False):
             cluster3_list.append(c)
     
     #calculate new centroids
-    
+    for c1 in cluster1_list:
+        converge_current_value_1_x += x[c1]
+        converge_current_value_1_y += y[c1]
+    converge_current_value_1_x = converge_current_value_1_x/converge_current_value_1_x.len()
+    converge_current_value_1_y = converge_current_value_1_y/converge_current_value_1_y.len()    
 
-        
+    for c2 in cluster2_list:
+        converge_current_value_2_x += x[c2]
+        converge_current_value_2_y += y[c2]
+    converge_current_value_2_x = converge_current_value_2_x/converge_current_value_2_x.len()
+    converge_current_value_2_y = converge_current_value_2_y/converge_current_value_2_y.len() 
+
+    for c3 in cluster3_list:
+        converge_current_value_3_x += x[c3]
+        converge_current_value_3_y += y[c3]
+    converge_current_value_3_x = converge_current_value_3_x/converge_current_value_3_x.len()
+    converge_current_value_3_y = converge_current_value_3_y/converge_current_value_3_y.len()     
+
+    if (converge_prev_value_1_x == converge_current_value_1_x) and (converge_prev_value_1_y == converge_current_value_1_y) and (converge_prev_value_2_x == converge_current_value_2_x) and (converge_prev_value_2_y == converge_current_value_2_y) and (converge_prev_value_3_x == converge_current_value_3_x) and (converge_prev_value_3_y == converge_current_value_3_y):
+        converge = True
+
+    print("Iteration ", iteration)
+
+    print("Cluster 1: ", *iter(cluster1_list), sep=' ')
+    print("Centroid: (", converge_current_value_1_x, ", ", converge_current_value_1_y, ")")
+
+    print("Cluster 2: ", *iter(cluster2_list), sep=' ')
+    print("Centroid: (", converge_current_value_2_x, ", ", converge_current_value_2_y, ")")
+
+    print("Cluster 3: ", *iter(cluster3_list), sep=' ')
+    print("Centroid: (", converge_current_value_3_x, ", ", converge_current_value_3_y, ")")
+
+    iteration += 1
 
 
-
-print(x)
-print(y)
